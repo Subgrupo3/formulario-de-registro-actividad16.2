6e4e09d14f587ab082c04a8167f50b8487a3ab00
valentinadinardi <138677905+valentinadinardi@users.noreply.github.com>
2023-10-18 11:37:22 -0300

    Validaciones

--- js/validacion.js
+++ js/validacion.js
@@ -1,115 +1,122 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
-
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
-}
-
-function confContraseña() {
-    var password1 = document.getElementById("password1").value;
-    var password2 = document.getElementById("password2").value;
-    var terminos = document.getElementById("terminos"); // Asegúrate de obtener el elemento del checkbox correctamente
-
-    if (terminos.checked) {
-        if (password2 === password1 && password1.length >= 6) {
-            showAlertSuccess();
-        } else {
-            showAlertError();
-            event.preventDefault();
-        }
-    } else {
-        showAlertError();
-        event.preventDefault();
-    }
-}
-
 document.addEventListener("DOMContentLoaded", function() {
-    let form = document.getElementById('passwordForm');
-  
-    let email = document.getElementById('email');
-    let password1 = document.getElementById('password1');
-    let password2 = document.getElementById('password2');
-    let terminos = document.getElementById('terminos');
-  
-    form.addEventListener("submit", function(event) {
-      let valid = true;
-  
-      if (!email.value || !email.validity.valid) {
-        valid = false;
-        email.classList.add('is-invalid');
-      } else {
-        email.classList.remove('is-invalid');
-      }
-  
-      if (!password1.value || password1.value.length < 6) {
-        valid = false;
-        password1.classList.add('is-invalid');
-      } else {
-        password1.classList.remove('is-invalid');
-      }
-  
-      if (!password2.value || password1.value !== password2.value || password2.value.length < 6) {
-        valid = false;
-        password2.classList.add('is-invalid');
-      } else {
-        password2.classList.remove('is-invalid');
-      }
-  
-      if (!terminos.checked) {
-        valid = false;
-        terminos.classList.add('is-invalid');
-      } else {
-        terminos.classList.remove('is-invalid');
-      }
-  
-      if (!valid) {
-        event.preventDefault(); // prevent form from submitting
-        document.getElementById('alert-danger').classList.add('show');
-      } else {
-        document.getElementById('alert-success').classList.add('show');
-      }
-    });
-  
-    email.addEventListener("input", function() {
-      if (email.validity.valid) {
-        email.classList.remove('is-invalid');
-      } else {
-        email.classList.add('is-invalid');
-      }
-    });
-  
-    password1.addEventListener("input", function() {
-      if (password1.value.length >= 6) {
-        password1.classList.remove('is-invalid');
-      } else {
-        password1.classList.add('is-invalid');
-      }
-  
-      if (password2.value !== password1.value || !password1.validity.valid) {
-        password2.classList.add('is-invalid');
-      } else {
-        password2.classList.remove('is-invalid');
-      }
-    });
-  
-    password2.addEventListener("input", function() {
-      if (password1.value === password2.value && password2.value.length >= 6) {
-        password2.classList.remove('is-invalid');
-      } else {
-        password2.classList.add('is-invalid');
-      }
-    });
-  
-    terminos.addEventListener("change", function() {
-      if (terminos.checked) {
-        terminos.classList.remove('is-invalid');
-      } else {
-        terminos.classList.add('is-invalid');
-      }
-    });
-  
-  });
-  
+// Selecciona el formulario por su id "formRegistro"
+let form = document.querySelector("form");
 
-  
+form.addEventListener("submit", function (event) {
+  let obligatorios = document.getElementsByClassName("form-control");
+
+  for (let i = 0; i < obligatorios.length; i++) {
+    if (obligatorios[i].value === "") {
+      obligatorios[i].classList.add("is-invalid"); // Cambia la clase para aplicar el estilo de validación
+      event.preventDefault(); // Evita que se envíe el formulario
+    }
+    else{
+        obligatorios[i].classList.remove("is-invalid");
+    }
+  }
+   const inputFields = document.getElementsByClassName("form-control");
+
+   for (let i = 0; i < inputFields.length; i++) {
+     inputFields[i].addEventListener("input", function (event) {
+       if (inputFields[i].value === "") {
+         inputFields[i].classList.add("is-invalid"); // Agrega la clase de validación
+       } else {
+         inputFields[i].classList.remove("is-invalid"); // Elimina la clase de validación
+       }
+     });
+   }
+
+    const contrasenia = document.getElementById("password1");
+    const psw2 = document.getElementById("password2");
+    
+    if (contrasenia.value != psw2.value) {
+        psw2.classList.add("is-invalid");
+    } else {
+        psw2.classList.remove("is-invalid"); // Elimina la clase de validación
+      
+    }
+
+    var terminos = document.getElementById('terminos');
+    var tituloTerminos = document.getElementById("terminosTitulo");
+
+    if(!(terminos.checked)){
+        terminos.classList.add("is-invalid");
+        tituloTerminos.classList.add("is-invalid");
+        tituloTerminos.classList.add("text-danger");
+    } else{
+        terminos.classList.remove("is-invalid");
+        tituloTerminos.classList.remove("is-invalid");
+        tituloTerminos.classList.remove("text-danger");
+        terminos.classList.add("text-success");
+    }
+
+});
+
+var terminos = document.getElementById('terminos');
+terminos.addEventListener("change", function(){
+    if(!(terminos.checked)){
+        terminos.classList.add("is-invalid");
+        tituloTerminos.classList.add("is-invalid");
+        tituloTerminos.classList.add("text-danger");
+    } else{
+        terminos.classList.remove("is-invalid");
+        tituloTerminos.classList.remove("is-invalid");
+        tituloTerminos.classList.remove("text-danger");
+        terminos.classList.add("text-success");
+    }
+})
+
+const email = document.getElementById("email");
+const emailError = document.querySelector("#mail + span.error");
+
+email.addEventListener("input", function (event) {
+  // Cada vez que el usuario escribe algo, verificamos si
+  // los campos del formulario son válidos.
+
+  if (email.validity.valid) {
+    // En caso de que haya un mensaje de error visible, si el campo
+    // es válido, eliminamos el mensaje de error.
+    emailError.innerHTML = ""; // Restablece el contenido del mensaje
+    emailError.className = "error"; // Restablece el estado visual del mensaje
+  } else {
+    // Si todavía hay un error, muestra el error exacto
+    showError();
+  }
+  if (!email.validity.valid) {
+   showError();
+    // Eevitamos que se envíe el formulario cancelando el evento
+    event.preventDefault();
+  }
+
+});
+
+const contrasenia = document.getElementById("password1");
+  const contraseniaError = document.querySelector("#password1 + span.error");
+
+  if (contrasenia.validity.tooShort) {
+    // Si los datos son demasiado cortos
+    // muestra el mensaje de error siguiente.
+    contraseniaError.textContent =
+      "La contaseña debe tener al menos ${contrasenia.minLength} caracteres; ha introducido ${contrasenia.value.length }.";
+  }
+
+function showError() {
+    if (email.validity.valueMissing) {
+      // Si el campo está vacío
+      // muestra el mensaje de error siguiente.
+      emailError.textContent =
+        "Debe introducir una dirección de correo electrónico.";
+    } else if (email.validity.typeMismatch) {
+      // Si el campo no contiene una dirección de correo electrónico
+      // muestra el mensaje de error siguiente.
+      emailError.textContent =
+        "El valor introducido debe ser una dirección de correo electrónico.";
+    } else if (email.validity.tooShort) {
+      // Si los datos son demasiado cortos
+      // muestra el mensaje de error siguiente.
+      emailError.textContent =
+        "El correo electrónico debe tener al menos ${ email.minLength } caracteres; ha introducido ${ email.value.length }.";
+    }
+}  
+
+})
