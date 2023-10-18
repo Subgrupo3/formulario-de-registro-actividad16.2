020ca92de0068a0258db201b428fa7ee22002398
agustindiaz2021 <agusdiaz654321@gmail.com>
2023-10-18 10:12:49 -0300

    cositas

--- js/validacion.js
+++ js/validacion.js
@@ -24,5 +24,92 @@ function confContraseña() {
     }
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    let form = document.getElementById('passwordForm');
+  
+    let email = document.getElementById('email');
+    let password1 = document.getElementById('password1');
+    let password2 = document.getElementById('password2');
+    let terminos = document.getElementById('terminos');
+  
+    form.addEventListener("submit", function(event) {
+      let valid = true;
+  
+      if (!email.value || !email.validity.valid) {
+        valid = false;
+        email.classList.add('is-invalid');
+      } else {
+        email.classList.remove('is-invalid');
+      }
+  
+      if (!password1.value || password1.value.length < 6) {
+        valid = false;
+        password1.classList.add('is-invalid');
+      } else {
+        password1.classList.remove('is-invalid');
+      }
+  
+      if (!password2.value || password1.value !== password2.value || password2.value.length < 6) {
+        valid = false;
+        password2.classList.add('is-invalid');
+      } else {
+        password2.classList.remove('is-invalid');
+      }
+  
+      if (!terminos.checked) {
+        valid = false;
+        terminos.classList.add('is-invalid');
+      } else {
+        terminos.classList.remove('is-invalid');
+      }
+  
+      if (!valid) {
+        event.preventDefault(); // prevent form from submitting
+        document.getElementById('alert-danger').classList.add('show');
+      } else {
+        document.getElementById('alert-success').classList.add('show');
+      }
+    });
+  
+    email.addEventListener("input", function() {
+      if (email.validity.valid) {
+        email.classList.remove('is-invalid');
+      } else {
+        email.classList.add('is-invalid');
+      }
+    });
+  
+    password1.addEventListener("input", function() {
+      if (password1.value.length >= 6) {
+        password1.classList.remove('is-invalid');
+      } else {
+        password1.classList.add('is-invalid');
+      }
+  
+      if (password2.value !== password1.value || !password1.validity.valid) {
+        password2.classList.add('is-invalid');
+      } else {
+        password2.classList.remove('is-invalid');
+      }
+    });
+  
+    password2.addEventListener("input", function() {
+      if (password1.value === password2.value && password2.value.length >= 6) {
+        password2.classList.remove('is-invalid');
+      } else {
+        password2.classList.add('is-invalid');
+      }
+    });
+  
+    terminos.addEventListener("change", function() {
+      if (terminos.checked) {
+        terminos.classList.remove('is-invalid');
+      } else {
+        terminos.classList.add('is-invalid');
+      }
+    });
+  
+  });
+  
 
   
